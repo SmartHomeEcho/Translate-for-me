50bdec5a8ed2f07a2c96a8cfb3f39ba67476a578
Roland Ádám <adamroland86@outlook.com>
2021-09-11 15:28:44 +0200

    Jól haladtam

---
+++ Fordító/AlkalmazasCache.cs
@@ -0,0 +1,16 @@
+﻿namespace Fordító
+{
+    class AlkalmazasCache
+    {
+        //Trial API kulcs és minden más is csak olvasható!!
+        public static string API { get; } = @"trnsl.1.1.20210911T104318Z.643311325218e5f6.84ea491c36faa404a5c344fe4f96b9e77ad3e47e";
+        //Nyelvek lekérése a yandexről Postmannel érdemes tesztelni a működését.
+        //1. Postman--->get: bemásolni a címet, hogy a megfelelő kulcs és egy tetszőleges nyelvet beírsz. Ezzel lehet tesztelni a lekérés működését
+        //2. https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20210911T104318Z.643311325218e5f6.84ea491c36faa404a5c344fe4f96b9e77ad3e47e&ui=hu
+        //3. az alábbi mindhárom esetben így lehet tesztelni a lekérdezéseket, csak be kell helyettesíteni.
+        public static string LehetsegesNyelvek { get; } = @"https://translate.yandex.net/api/v1.5/tr.json/getLangs?key={0}&ui={1}";
+        public static string ForrasNyelvDetektalas { get; } = @"https://translate.yandex.net/api/v1.5/tr.json/detect?key={0}&text={1}";
+        public static string  NyelvForditas { get; } = @"https://translate.yandex.net/api/v1.5/tr.json/translate?key={0}&text={1}&lang={2}";
+    }
+    
+}
--- Fordító/Form1.cs
+++ Fordító/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,116 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace Fordító
 {
     public partial class Form1 : Form
     {
+        List<string> NyelvekLista = new List<string>();
         public Form1()
         {
             InitializeComponent();
+        }
+        private IRestResponse KeresSzolgaltatas(string UrlCim)
+        {
+            //Kliens példányosítása, létrehozása, AlkalmazasCache adatainak meghívása
+            var Kliens = new RestClient()
+            {
+
+                BaseUrl = new Uri(UrlCim)
+            };
+            //Keresés metódus példányosítása
+            var Keres = new RestRequest()
+            {
+                Method = Method.GET
+            };
+            //Kliensen a keresés lefuttatása, amely a választ eredményezi
+            return Kliens.Execute(Keres);
+        }
+       
+        //Szokásos bezárás kérdéssel
+        private void Bezaras_Gomb_Click(object sender, EventArgs e)
+        {
+            {
+                if (MessageBox.Show("Biztosan ki szeretne lépni?", "Biztosan?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                {
+                    return;
+                }
+                else
+                {
+                    try
+                    {
+                        Application.Exit();
+                    }
+                    catch
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+        //
+        private void NyelvFelismereseGomb_Click(object sender, EventArgs e)
+        {
+
+            var Valasz= KeresSzolgaltatas(string.Format(AlkalmazasCache.ForrasNyelvDetektalas, AlkalmazasCache.API, ForditandoTextBox.Text));
+             //A válasz kimentése egy Szótárba (IDictionary)
+             var Szotar= JsonConvert.DeserializeObject<IDictionary>(Valasz.Content);
+            //Statuszkód változó ha a szótár "code" értéke 200
+            var StatuszKod = Szotar["code"].ToString();
+            //Ha az érték 200 a labelbe kiírja a felismert nyelvet.
+            if (StatuszKod.Equals("200"))
+            {
+                FelismertNyelvLabel.Text = Szotar["lang"].ToString();
+            }
+        }
+
+        private void AC_Click(object sender, EventArgs e)
+        {
+            var Valasz = KeresSzolgaltatas(string.Format(AlkalmazasCache.LehetsegesNyelvek, AlkalmazasCache.API, FelismertNyelvLabel.Text));
+            var Szotar = JsonConvert.DeserializeObject<IDictionary>(Valasz.Content);
+            foreach (DictionaryEntry entry in Szotar)
+            {
+                if (entry.Key.Equals("langs"))
+                {
+                    var LehetsegesKonvertalas = (JObject)entry.Value;
+                    NyelvekLista = new List<string>();
+                    NyelvValaszto.Items.Clear();
+                    foreach (var Lang in LehetsegesKonvertalas)
+                    {
+                        if (!Lang.Equals(FelismertNyelvLabel.Text))
+                        {
+                            NyelvValaszto.Items.Add(Lang.Value);
+                            NyelvekLista.Add(Lang.Key);
+                        }
+                    }
+                }
+            }
+        }
+
+       
+
+        private void Fordítás_Gomb_Click(object sender, EventArgs e)
+        {
+            var Valasz = KeresSzolgaltatas(string.Format(AlkalmazasCache.NyelvForditas, AlkalmazasCache.API, ForditandoTextBox.Text, NyelvekLista[NyelvValaszto.SelectedIndex] ));
+            var Szotar = JsonConvert.DeserializeObject<IDictionary>(Valasz.Content);
+            var StatuszKod = Szotar["code"].ToString();
+            if (StatuszKod.Equals("200"))
+            {
+               LeforditottSzoveg.Text= string.Join(",", Szotar["text"]);
+            }
+        }
+
+        private void InformacioGomb_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1. Legfelső szövegdobozban azz meg a fordítandó szöveget\n" +
+                "2. Nyelv felismerése gombra kattintva kiírja a bal alsó sarokban a detektált nyelvet\n" +
+                "3. Ezután Lehetséges nyelvek feltöltése gombra kell kattintani, amely következtében az alatta lévő lenyíló listát feltölti\n" +
+                "4. Listábol válaszd ki a kívánt nyelvet, majd fordítás gomb!:).\n"
+                , "Információ (ha nem tudod használni kérdezz!)", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
